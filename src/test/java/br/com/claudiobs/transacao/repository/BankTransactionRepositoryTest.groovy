dfa7cf6027dde5df039f76f5c50012a576053687
Claudio Santos <claudiobs07@gmail.com>
2018-03-18 19:39:08 -0300

    add: bank transfer repository

---
+++ src/test/java/br/com/claudiobs/transacao/repository/BankTransactionRepositoryTest.groovy
@@ -0,0 +1,29 @@
+package br.com.claudiobs.transacao.repository
+
+import br.com.claudiobs.transacao.TransacaoApplicationTests
+import br.com.claudiobs.transacao.domain.BankTransfer
+import org.springframework.beans.factory.annotation.Autowired
+
+import java.time.LocalDate
+
+class BankTransactionRepositoryTest extends TransacaoApplicationTests {
+    
+    @Autowired
+    BankTransferRepository repository
+    
+    def "should save and get a banck transaction"() {
+        given:
+            def bankTransaction =  new BankTransfer(
+                    sourceAccount: 123456,
+                    destinationAccount: 654321,
+                    amount: 12.5,
+                    tax: 0.3,
+                    date: LocalDate.now()
+            )
+        when:
+            def persisted = repository.save(bankTransaction)
+        then:
+            persisted
+    }
+
+}
