64308b9ed81cc63bca7cd5c3f2016c179a1a07db
Claudio Santos <claudiobs07@gmail.com>
2018-03-18 21:32:47 -0300

    add: transfer bank service

--- src/main/java/br/com/claudiobs/transacao/controller/BankTransferController.java
+++ src/main/java/br/com/claudiobs/transacao/controller/BankTransferController.java
@@ -1,6 +1,7 @@
 package br.com.claudiobs.transacao.controller;
 
 import br.com.claudiobs.transacao.domain.BankTransfer;
+import br.com.claudiobs.transacao.service.BankTransferService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,11 +20,17 @@ public class BankTransferController {
 
     private final Logger log = LoggerFactory.getLogger(this.getClass());
 
+    private BankTransferService bankTransferService;
+
+    BankTransferController(BankTransferService bankTransferService) {
+        this.bankTransferService = bankTransferService;
+    }
+
     @ResponseStatus(CREATED)
     @PostMapping
     BankTransfer create(@Valid @RequestBody BankTransfer bankTransfer) {
         log.info("creating bankTransfer=" + bankTransfer);
-        //TODO request service
+        bankTransferService.create(bankTransfer);
         return null;
     }
 }
---
+++ src/main/java/br/com/claudiobs/transacao/service/BankTransferService.java
@@ -0,0 +1,12 @@
+package br.com.claudiobs.transacao.service;
+
+import br.com.claudiobs.transacao.domain.BankTransfer;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BankTransferService {
+
+    public BankTransfer create(BankTransfer bankTransfer) {
+        return null;
+    }
+}
