62a2902d781d2ae00d0d21f69b0301260e351a93
Claudio Santos <claudiobs07@gmail.com>
2018-03-21 20:02:28 -0300

    fix: day tax calculator

--- src/test/java/br/com/claudiobs/transacao/service/tax/DayTaxCalculatorTest.groovy
+++ src/test/java/br/com/claudiobs/transacao/service/tax/DayTaxCalculatorTest.groovy
@@ -10,26 +10,26 @@ class DayTaxCalculatorTest extends Specification {
     @Unroll
     def "check tax calculator valid when dayToBankTransfer=#dayToBankTransfer then expect=#expected"() {
         when:
-            def valid = taxCalculator.isValid(dayToBankTransfer, new BigDecimal(amount))
+            def valid = taxCalculator.isValid(dayToBankTransfer, new BigDecimal(123.00))
         then:
             valid == expected
         where:
-            dayToBankTransfer   | amount    | expected
-            0l                  | 123.00    | true
-            1l                  | 123.00    | false
-            20l                 | 123.00    | false
+            dayToBankTransfer   | expected
+            0l                  | true
+            1l                  | false
+            20l                 | false
     }
     
     @Unroll
-    def "should"() {
+    def "given a amount=#amount should get tax=#tax"() {
         when:
             def tax = taxCalculator.getTax(0l, new BigDecimal(amount))
         then:
             tax == expected
         where:
             amount    | expected
-            100.00    | 106.0
-            200.00    | 212.0
-            1.00      | 4.06
+            100.00    | 6.0
+            200.00    | 9.0
+            1.00      | 3.03
     }
 }
